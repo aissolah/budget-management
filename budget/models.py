7e80590eca8b3b41e35665358595e1036d993084
Abderramanhe Issolah <aissolah@ACTIVSOFT.SKOLIA.COM>
2020-06-05 18:04:16 +0200

    FK a budget to a user

--- budget/models.py
+++ budget/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Budget(models.Model):
   name = models.CharField(max_length=250)
   month = models.IntegerField(blank=True)
   year = models.IntegerField(blank=True)
-  is_archived = models.BooleanField()
+  is_archived = models.BooleanField(default=False)
   initial_amount = models.DecimalField(max_digits=8, decimal_places=2)
   total_amount = models.DecimalField(max_digits=8, decimal_places=2)
-
+  user = models.ForeignKey(User,models.CASCADE)
 
 class Charge(models.Model):
   name = models.CharField(max_length=250)
