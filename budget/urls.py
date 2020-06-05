1105e91d6b363c72e4096cac08c8a3db1f4f0fa5
Abderramanhe Issolah <aissolah@ACTIVSOFT.SKOLIA.COM>
2020-06-05 16:50:22 +0200

    login and sign up

--- budget/urls.py
+++ budget/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home, budget_detail
+from .views import home, signup_user, login_user, logout_user, budgets, budget_detail
 urlpatterns = [
+	path('signup/', signup_user, name="signup_user"),
+	path('logout/', logout_user, name="logout_user"),
+	path('login/', login_user, name="login_user"),
     path('', home, name="home"),
-    path('<int:budget_id>', budget_detail, name="budget_detail"),
+    path('budgets/', budgets, name="budgets"),
+    path('budgets/<int:budget_id>', budget_detail, name="budget_detail"),
 ]
--- budget/views.py
+++ budget/views.py
@@ -1,10 +1,52 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Budget,Charge
-# Create your views here.
-def home(request):
-  budgets = Budget.objects.all()
-  return render(request, 'budget/home.html',{'budgets':budgets})
 
+from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+
+
+
+# ***Signup the user***
+def signup_user(request):
+	if request.method == "GET":
+		return render(request,'budget/signup_user.html',{'form':UserCreationForm})
+	else:
+		if request.POST["password1"] == request.POST["password2"]:
+			user = User.objects.create_user(request.POST["username"],password=request.POST["password1"])
+			login(request,user)
+			return redirect('budgets')
+		else:
+			return render(request,'budget/signup_user.html',{'form':UserCreationForm,'error':'Passwords did not match'})
+
+# ***Log the user in***
+def login_user(request):
+	if request.method == "GET":
+		return render(request,'budget/login_user.html',{'form':AuthenticationForm})
+	else:
+		user = authenticate(username=request.POST["username"],password=request.POST["password"])
+		if user is None:
+			return render(request,'budget/login_user.html',{'form':AuthenticationForm, 'error':'Invalid username or password'})
+		else:
+			login(request,user)
+			return redirect('budgets')
+
+# ***logout the user ***
+def logout_user(request):
+	if request.method == "POST":
+		logout(request)
+		return redirect("home")
+
+# ***Home page***
+def home(request):
+  return render(request, 'budget/home.html')
+
+# ***Shows all the budgets of the currently logged in user***
+def budgets(request):
+  budgets = Budget.objects.all()
+  return render(request, 'budget/budgets.html',{'budgets':budgets})
+
+# ***Detail page of a selected budget***
 def budget_detail(request, budget_id):
   budget = get_object_or_404(Budget,pk=budget_id)
   charges = Charge.objects.all().filter(budget=budget_id)
